fc4a061595909480a8cd5edca3d24476c9dde7b0
MicherGit <40839581+MicherGit@users.noreply.github.com>
2021-01-20 18:12:49 -0500

    updated main class name and author

--- src/main/java/net/zeta/cursedfarlands/ConcernedFarlandsUnusedMain.java
+++ src/main/java/net/zeta/cursedfarlands/ConcernedFarlandsUnusedMain.java
@@ -2,7 +2,7 @@ package net.zeta.cursedfarlands;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class ConcernedFarlandsUnusedMain implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
